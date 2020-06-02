e4f1efa32d908efe68442fea0c463b61c9f52e8a
최지호 <tnrwl4@naver.com>
2020-06-02 14:23:43 +0900

    총알클래스 생성
    - 총알 프리팹 만들기
    - 플레이어 총알 발사
    - 플레이어 보조비행기 생성
    - 보조비행기 자동공격

---
+++ Unity Project/Assets/_CJH/Script/PlayerFire.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFire : MonoBehaviour
+{
+    public GameObject bulletFactory;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Fire();   
+    }
+
+    private void Fire()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(bulletFactory, transform.GetChild(0));
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.GetChild(1).gameObject.SetActive(!transform.GetChild(1).gameObject.activeSelf);
+        }
+    }
+}
