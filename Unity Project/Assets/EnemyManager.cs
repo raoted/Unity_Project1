b790f31b435eb92cd441b229f8ab173d73933c03
최지호 <tnrwl4@naver.com>
2020-06-03 12:23:55 +0900

    에너미 생성
    - 에너미 프리팹 추가
    - 에너미매니저 추가
    - 랜덤한 스폰 포인트에 0,5초 ~ 2초 간격으로 에너미 프리팹 생성
    - 에너미와 플레이어가 충돌하면 둘 다 삭제.
    
    DestroyZone 생성
    - 카메라 외부에 충돌 레이어 설정
    - 충돌 레이어와 충돌한 Bullet과 Enemy는 삭제 처리

---
+++ Unity Project/Assets/EnemyManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public GameObject enemyFactory;
+    public int idx;
+    public GameObject[] spawnPoint;
+    float spawnTime = 1.0f;        //스폰타임
+    float curTime = 0.0f;          //누적타임
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawEnemy();    
+    }
+
+    private void SpawEnemy()
+    {
+        //몇초에 한번씩 이벤트 발동
+        //시간 누적타임으로 계산한다.
+        //게임에서 자수 사용됨.
+
+        curTime += Time.deltaTime;
+        if(curTime > spawnTime)
+        {
+            spawnTime = Random.Range(0.5f, 2.0f);
+            //누적된 시간을 0으로 초기하
+
+            curTime = 0.0f;
+
+
+            GameObject enemy = Instantiate(enemyFactory);
+            enemy.transform.position = spawnPoint[Random.Range(0, idx)].transform.position;
+        }
+    }
+}
